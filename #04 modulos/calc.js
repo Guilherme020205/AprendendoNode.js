d81df4a8f2afed4bbf93e3a8c467fe3c929e6775
Guilherme Cardozo Seemann <104910701+Guilherme020205@users.noreply.github.com>
2023-06-12 11:51:21 -0300

    Add files via upload

---
+++ #04 modulos/calc.js
@@ -0,0 +1,9 @@
+var somaFunc = require("./soma");
+var subFunc = require("./sub")
+var multFunc = require("./mult")
+var divFunc = require("./div")
+
+console.log(somaFunc(1,2));
+console.log(subFunc(10,5));
+console.log(multFunc(100,10));
+console.log(divFunc(1,2));
---
+++ #04 modulos/mult.js
@@ -0,0 +1,5 @@
+var mut = function(a, b) {
+    return a * b;
+}
+
+module.exports = mut;
